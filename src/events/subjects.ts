cbb858a04648ee566b76bcf4044f586babfbb63a
abfiker <new94abemelek@gmail.com>
2024-12-31 10:03:27 +0300

    Updates

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -1,9 +1,4 @@
 export enum Subjects {
   TicketCreated = 'ticket:created',
   TicketUpdated = 'ticket:updated',
-  // OrderUpdated = 'order:updated',
-  //   OrderCreated = 'order:created',
-  //   OrderCancelled = 'order:cancelled',
-  //   PaymentCreated = 'payment:created',
-  //   UserRegistered = 'user:registered',
 }
