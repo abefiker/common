5b2cd8b4ca454d4a2ff0869301d85ae4c4d9ed48
abfiker <new94abemelek@gmail.com>
2025-01-31 16:34:38 +0300

    Updates

---
+++ src/events/order-cancelled-event.ts
@@ -0,0 +1,11 @@
+import { OrderStatus } from './types/order-status';
+import { Subjects } from './subjects';
+export interface OrderCancelledEvent {
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    };
+  };
+}
