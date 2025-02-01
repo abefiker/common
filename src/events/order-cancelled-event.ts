5b9fd35ab70e542304212146e40a6c7aae560b10
abfiker <new94abemelek@gmail.com>
2025-02-01 15:16:43 +0300

    Updates

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -1,7 +1,7 @@
 import { OrderStatus } from './types/order-status';
 import { Subjects } from './subjects';
 export interface OrderCancelledEvent {
-  subject: Subjects.OrderCancelled;
+  subjects: Subjects.OrderCancelled;
   data: {
     id: string;
     version: number;
--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -1,7 +1,7 @@
 import { Subjects } from './subjects';
 import { OrderStatus } from './types/order-status';
 export interface OrderCreatedEvent {
-  subject: Subjects.OrderCreated;
+  subjects: Subjects.OrderCreated;
   data: {
     id: string;
     userId: string;
